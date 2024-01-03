cabf73445fd56ffa00d07907bfa6a7229bb6a40f
ergegananputra <ergegananputra@gmail.com>
2024-01-03 15:48:10 +0700

    new ui home, clean ui todo, clean ui card schedule

--- app/src/main/java/com/minizuure/todoplannereducationedition/surface/home/HomeFragment.kt
+++ app/src/main/java/com/minizuure/todoplannereducationedition/surface/home/HomeFragment.kt
@@ -1,16 +1,30 @@
 package com.minizuure.todoplannereducationedition.surface.home
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.minizuure.todoplannereducationedition.R
 import com.minizuure.todoplannereducationedition.databinding.FragmentHomeBinding
+import com.minizuure.todoplannereducationedition.first_layer.detail.DetailActivity
 
 
 class HomeFragment : Fragment() {
     private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+
+
+    private val launcherToAddTask = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            Log.d("HomeFragment", "onCreate: RESULT_OK")
+            //TODO: do something
+        }
+    }
 
 
     override fun onCreateView(
@@ -23,6 +37,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupEfabAddTask()
+    }
+
+    private fun setupEfabAddTask() {
+        binding.efabAddTask.setOnClickListener {
+            Toast.makeText(requireContext(), "TODO: add task clicked", Toast.LENGTH_SHORT).show()
+//            val intentToTaskForm = Intent(requireActivity(), xx::class.java)
+//            launcherToAddTask.launch(intentToTaskForm)
+        }
     }
 
 
