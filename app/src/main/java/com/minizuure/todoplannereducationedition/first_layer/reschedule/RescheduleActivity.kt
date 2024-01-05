4151c93dd2a435388cc7289cba5bea279f5c29c6
mfr03 <maulgantengmaks@gmail.com>
2024-01-05 18:14:55 +0700

    added reschedule

---
+++ app/src/main/java/com/minizuure/todoplannereducationedition/first_layer/reschedule/RescheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.minizuure.todoplannereducationedition.first_layer.reschedule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minizuure.todoplannereducationedition.R
+
+class RescheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reschedule)
+    }
+}
