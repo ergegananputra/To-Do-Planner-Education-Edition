7aaed3a88b425d0c03a1ac27ea9e10880f93db43
ergegananputra <ergegananputra@gmail.com>
2024-01-11 20:58:47 +0700

    Delete Task

--- app/src/main/java/com/minizuure/todoplannereducationedition/services/database/task/TaskTable.kt
+++ app/src/main/java/com/minizuure/todoplannereducationedition/services/database/task/TaskTable.kt
@@ -8,37 +8,12 @@ import androidx.room.PrimaryKey
  * [Last edited by ergegananputra 11 January 2024]
  *
  *
- * Penjelasan dan Tata Cara Penggunaan Class [TaskTable] terutama [dateISO8601].
+ * Kelas ini tidak menyimpan Memo, Quiz, dan To-Pack. Kelas ini hanya menyimpan data dari task yang dibuat oleh user.
+ * Memo, Quiz, dan To-Pack disimpan di kelas [NotesTaskTable].
  *
  *
- * Penjelasan [dateISO8601]
- *
- *
- * Pada pembuatan [dateISO8601] adalah null, hal ini diperuntukkan untuk menghemat memori.
- * Kolom [dateISO8601] harus diisi ketika user menambahkan NoteTask atau TodoTask untuk membedakan
- * Task yang repetitif identik dan Task yang sudah memiliki NoteTask atau TodoTask.
- *
- *
- * Pengmabilan data Task pada *HomeFragment*, *TodoFragment*, dan management lainnya yang berhubungan
- * dengan task dilakukan dengan cara mencari task berdasarkan [indexDay] dan [dateISO8601].
- * Jika [dateISO8601] null, maka pencarian task hanya berdasarkan [indexDay] saja.
- *
- *
- * Set value [dateISO8601]
- *
- *
- * Untuk mengisi [dateISO8601], pastikan user membuka Task pada date yang bersangkutan.
- *
- *
- * Sebagai contoh di *TodoFragment.kt* :
- *
- *
- * Ketika user mengarahkan tab ke tanggal 19 Januari 2023 dan membuka Task,
- * maka sebelum membuka Task, pastikan telah  mendapatkan tanggal 19 Januari 2023 tersebut.
- * Dengan demikian [dateISO8601] dapat diisi dengan tanggal 19 Januari 2023 dengan FORMAT ISO8601.
- * Gunakan DatetimeAppManager untuk melakukan konversi!
- *
- *
+ * Maka dari itu untuk mengambil data Memo, Quiz, dan To-Pack, kita harus mengambil data dari
+ * kelas [NotesTaskTable] dengan menggunakan id dari task yang dibuat oleh user.
  *
  *
  * NOTE: The startTime and endTime must be updated when the user changes the time of the session.
@@ -49,7 +24,6 @@ import androidx.room.PrimaryKey
  * @param title The title of the task.
  * @param indexDay The day the task is active. Stored in integer format.
  * @param sessionId to make relation with session, it's not must because sometimes user choose custom session.
- * @param dateISO8601 The date the task is active. Stored in ISO8601 format. NOTE: Filled when there is NoteTask or TodoTask.
  * @param updatedAt The time the task is updated.
  * @param isCustomSession to check if user choose custom session or not.
  * @param startTime The time the task starts.
@@ -74,9 +48,6 @@ data class TaskTable(
 
     @ColumnInfo(name = "session_id")
     var sessionId : Long = 0,
-
-    @ColumnInfo(name = "date_iso8601")
-    var dateISO8601 : String? = null,
 
     @ColumnInfo(name = "updated_at")
     var updatedAt: Long = System.currentTimeMillis(),
